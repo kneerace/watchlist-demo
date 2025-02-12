02d622aa58f2f96ae8452f5fd80c8245ec348c48
kneerace <kneerace@gmail.com>
2025-02-12 14:41:47 -0500

    watlistController-updates after segregation of MVC i.e. model , vew and controller

--- src/main/java/com/openclassrooms/watchlist/controller/WatchlistController.java
+++ src/main/java/com/openclassrooms/watchlist/controller/WatchlistController.java
@@ -1,7 +1,12 @@
-package com.openclassrooms.watchlist;
+package com.openclassrooms.watchlist.controller;
 
+import com.openclassrooms.watchlist.domain.WatchlistItem;
+import com.openclassrooms.watchlist.exception.DuplicateTitleException;
+import com.openclassrooms.watchlist.service.WatchlistService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +22,16 @@ import java.util.Map;
 @Controller
 public class WatchlistController {
 
-    private List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
-    private static int index = 1;
+    /*private List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
+    private static int index = 1;*/
+
+//    @Autowired  // this is field injection
+    WatchlistService watchlistService;
+
+    @Autowired // this is constructor injection
+    public WatchlistController(WatchlistService watchlistService) {
+        this.watchlistService = watchlistService;
+    } // end constructor
     @GetMapping("/watchlist")
     public ModelAndView getWatchlist() {
 //        watchlistitems.clear();
@@ -31,32 +44,17 @@ public class WatchlistController {
         String viewName = "watchlist";
         Map<String, Object> model = new HashMap<String, Object>();
 //        System.out.println(watchlistitems); //--------
-        model.put("watchlistItems", watchlistItems);
-        model.put("numberOfMovies", watchlistItems.size());
+        model.put("watchlistItems", watchlistService.getWatchlistItems());
+        model.put("numberOfMovies", watchlistService.getWatchlistItemSize());
         return new ModelAndView(viewName, model);
     }
-
-   /* @GetMapping("/watchlistItemForm")
-    public ModelAndView showWatchlistItemForm(@RequestParam(required = false) Integer id) {
-
-        String viewName = "watchlistItemForm";
-        Map<String,Object> model = new HashMap<String,Object>();
-        WatchlistItem watchlistitem = findWatchlistItemById(id);
-        if(watchlistitem != null) {
-            model.put("watchlistItem", watchlistitem);
-        }
-        else {
-            model.put("watchlistItem", new WatchlistItem());
-        }
-        return new ModelAndView(viewName,model);
-    } // end showWatchlistItemForm*/
-
     @GetMapping("/watchlistItemForm")
     public ModelAndView showWatchlistItemForm(@RequestParam(required = false) Integer id) {
         ModelAndView mav = new ModelAndView("watchlistItemForm");  // creating modelandview
         WatchlistItem watchlistItem;
         if (id != null) {
-            watchlistItem = findWatchlistItemById(id);
+//            watchlistItem = findWatchlistItemById(id);
+            watchlistItem = watchlistService.findWatchlistItemById(id);
             if(watchlistItem == null) {
                 return new ModelAndView("watchlistItemForm");
             }}
@@ -67,41 +65,27 @@ public class WatchlistController {
         return mav;
     } // end showWatchlistItemForm
 
-    private WatchlistItem findWatchlistItemById(Integer id) {
-            for (WatchlistItem watchlistitem : watchlistItems) {
-                if (watchlistitem.getId().equals(id)) {
-                    return watchlistitem;
-                } // end if
-            } // end for
-            return null;
-    } // end findWatchlistItemById
-
     @PostMapping("/watchlistItemForm")
     public ModelAndView submitWatchlistItemForm(@Validated WatchlistItem watchlistItem, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-           /* System.out.println("BindingResult Errors:");
+            System.out.println("BindingResult Errors:");
             for (FieldError error : bindingResult.getFieldErrors()) {
                 System.out.println("Field: " + error.getField());
                 System.out.println("Error: " + error.getDefaultMessage());  // Get the message
                 System.out.println("Rejected Value: " + error.getRejectedValue()); // See what was submitted
                 System.out.println("--------------------");
-            }*/
+            }
             ModelAndView mav = new ModelAndView("watchlistItemForm");
             mav.addObject("watchlistItem", watchlistItem);
             return mav;
         }
-
-        WatchlistItem existingWatchlistItem = findWatchlistItemById(watchlistItem.getId());
-        if (existingWatchlistItem == null) {
-            watchlistItem.setId(index++);
-            watchlistItems.add(watchlistItem);
+        try {
+            watchlistService.addOrUpdateWatchlistItem(watchlistItem);
         }
-        else {
-            existingWatchlistItem.setTitle(watchlistItem.getTitle());
-            existingWatchlistItem.setRating(watchlistItem.getRating());
-            existingWatchlistItem.setPriority(watchlistItem.getPriority());
-            existingWatchlistItem.setComment(watchlistItem.getComment());
+        catch (DuplicateTitleException e) {
+            bindingResult.rejectValue("title", "error.watchlistItem", "Title already exists in the Watchlist");
+            return new ModelAndView("watchlistItemForm");
         }
 
         RedirectView redirect = new RedirectView();
